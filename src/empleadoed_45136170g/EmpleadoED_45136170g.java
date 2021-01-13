dd5ae20bc5d6a2e155421b3503edd660e16068fb
Alba Marrades <marrades.alba@gmail.com>
2021-01-13 19:40:24 +0100

    versión final con javadoc DNI 45136170G

--- src/empleadoed_45136170g/EmpleadoED_45136170g.java
+++ src/empleadoed_45136170g/EmpleadoED_45136170g.java
@@ -2,17 +2,39 @@ package empleadoed_45136170g;
 
 import java.util.*;
 
+/**
+ * Ejercicio práctico examen EVAL 1 ED
+ *
+ * @author ALBA MARRADES DNI 45136170G
+ * @version 1.0
+ */
 public class EmpleadoED_45136170g {
 
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
 
+    /**
+     * Constructor con tres parámetros
+     *
+     * @param nombreCompleto nombre completo en mayúsculas
+     * @param dni documento nacional de identidad con letra y sin espacios
+     */
     public EmpleadoED_45136170g(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
 
+    /**
+     * Calcula el salario bruto mensual en función del salario base por hora y
+     * las horas extras El precio por hora de la hora extra se determina con la
+     * constante FACTOR_HORA_EXTRA
+     *
+     * @param horasExtra número de horas extra dedicadas redondeado a la baja
+     * (entero)
+     *
+     * @return devuelve el salario bruto mensual en euros con dos decimales
+     */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora
