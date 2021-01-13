40ec77eef94caf37582fe46e3e0912e4357303b9
Alba Marrades <marrades.alba@gmail.com>
2021-01-13 19:32:30 +0100

    código sin javadoc DNI 45136170G

---
+++ src/empleadoed_45136170g/EmpleadoED_45136170g.java
@@ -0,0 +1,31 @@
+package empleadoed_45136170g;
+
+import java.util.*;
+
+public class EmpleadoED_45136170g {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public EmpleadoED_45136170g(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora
+                * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    public static void main(String[] args) {
+        EmpleadoED_45136170g emp = new EmpleadoED_45136170g("ALBA MARRADES", "45136170G");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra)
+                + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+}
